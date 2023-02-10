ca4edab0d17118098ab103faf15e616d27cca022
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-02-10 14:34:10 -0300

    Lidando com o populamento do Banco de Dados e utilizamos um facilitador que o laravel oferece por meio das Factories e Seeders

--- olw/app/Models/Company.php
+++ olw/app/Models/Company.php
@@ -4,15 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Company extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'comission_rate',
         'name'
     ];
+
+    public function sellers(): HasMany
+    {
+        return $this->hasMany(Seller::class);
+    }
     
 }
